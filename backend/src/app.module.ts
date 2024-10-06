d0ed3b9f21b5f719e5b831f1a69907dfe3c8dd4f
Mesa <66703097+RedOwlllll@users.noreply.github.com>
2024-10-06 22:34:26 +1300

    backend
    finished backend that is connected to mongodb. env file for backend should be for mongodb link and env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the ConfigModule available globally
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI), // Use environment variable for MongoDB connection
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
