10a775f12361893b8c054f3f0f69192933d319c2
tley4 <105393760+tley4@users.noreply.github.com>
2024-10-08 21:48:45 +1300

    Created css for the submission page

--- speed/src/app/submission/page.tsx
+++ speed/src/app/submission/page.tsx
@@ -4,81 +4,80 @@ const page = () => {
   return (
     <>
       <h1>Submission Form</h1>
-      <div>This page will display the submission form for articles</div>
-      <form className="max-w-md mx-auto">
-        <div className="relative z-0 w-full mb-5 group">
+      <form className="article-form">
+        <div className="form-field">
           <input
             type="text"
             name="floating_title"
             id="floating_title"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             placeholder=" "
             required
           />
           <label
             htmlFor="floating_title"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="form-label"
           >
             Article Title
           </label>
         </div>
 
-        <div className="relative z-0 w-full mb-5 group">
+        <div className="form-field">
           <textarea
             name="floating_abstract"
             id="floating_abstract"
             rows={5}
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             placeholder=" "
             required
           ></textarea>
           <label
              htmlFor="floating_abstract"
-             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+             className="form-label"
           >
-              Abstract
-            </label>
+            Abstract
+          </label>
         </div>
 
-        <div className="relative z-0 w-full mb-5 group">
+        <div className="form-field">
           <input
             type="text"
             name="floating_doi"
             id="floating_doi"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             placeholder=" "
             required
           />
           <label
             htmlFor="floating_doi"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="form-label"
           >
             DOI (Digital Object Identifier)
           </label>
         </div>
 
-        <div className="relative z-0 w-full mb-5 group">
+        <div className="form-field">
           <input
             type="text"
             name="floating_keywords"
             id="floating_keywords"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             placeholder=" "
             required
           />
           <label
             htmlFor="floating_keywords"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="form-label"
           >
             Keywords
           </label>
         </div>
 
-        <div className="relative z-0 w-full mb-5 group">
+        <div className="form-field">
           <select
             name="floating_article_type"
             id="floating_article_type"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             required
           >
             <option value="" disabled selected>
@@ -87,45 +86,44 @@ const page = () => {
             <option value="Research Article">Research Article</option>
             <option value="Review Article">Review Article</option>
             <option value="Case Study">Case Study</option>
-            <option value="Short Communication">Short Communication</option>
           </select>
           <label
             htmlFor="floating_article_type"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="form-label"
           >
             Article Type
           </label>
         </div>
 
-        <div className="relative z-0 w-full mb-5 group">
+        <div className="form-field">
           <input
             type="date"
             name="floating_publication_date"
             id="floating_publication_date"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             placeholder=" "
             required
           />
           <label
             htmlFor="floating_publication_date"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="form-label"
           >
             Publication Date
           </label>
         </div>
 
-        <div className="relative z-0 w-full mb-5 group">
+        <div className="form-field">
           <input
             type="text"
             name="floating_author"
             id="floating_author"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="form-input"
             placeholder=" "
             required
           />
           <label
             htmlFor="floating_author"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            className="form-label"
           >
             Author
           </label>
@@ -133,7 +131,7 @@ const page = () => {
 
         <button
           type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="submit-button"
         >
           Submit
         </button>
